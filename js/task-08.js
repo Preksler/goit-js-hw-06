8ae30db1f5a2eb8b5dcfb0a33d4ba63806e9ef64
Preksler <preksler@gmail.com>
2022-04-20 15:30:07 +0300

    finish task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    if(email.value === '' || password.value === '') {
+        alert('Все поля должны быть заполнены.');
+    }
+    // const formData = new FormData({
+    //     email: event.email.value,
+    //     password: event.password.value
+    // });
+    const user = {
+        email: email.value,
+        password: password.value
+    };
+    console.log(user);
+    form.reset();
+});
