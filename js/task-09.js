ec068d402988962d1fe4840ef5ef65e0eaad7489
Preksler <preksler@gmail.com>
2022-04-20 15:36:42 +0300

    finish task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorCode = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  colorCode.textContent = body.style.backgroundColor;
+});
