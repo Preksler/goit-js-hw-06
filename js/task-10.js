c8d9a04f1bf4bc0a7130b022a1481631b4e30224
Preksler <preksler@gmail.com>
2022-04-21 13:53:10 +0300

    finish task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputNumber: document.querySelector('#controls input'),
+  btnCreate: document.querySelector('button[data-create]'),
+  btnDestroy: document.querySelector('button[data-destroy]'),
+  divBox: document.querySelector('#boxes'),
+}
+
+let size = 30;
+
+const handleClick = (e) => {
+  const value = e.currentTarget.value;
+  refs.btnCreate.addEventListener('click', () => {
+    createBoxes(value);
+  });
+};
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount.length; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    size += 10;
+    div.style.backgroundColor = getRandomHexColor();
+    refs.divBox.append(div);
+  }
+}
+
+function destroyBoxes() {
+  refs.divBox.innerHTML = '';
+};
+
+refs.inputNumber.addEventListener('input', handleClick);
+refs.btnDestroy.addEventListener('click', destroyBoxes);
